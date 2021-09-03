0290170cd99463d085074cb135756139d9b0c896
laco-dev <laco2951@gmail.com>
2021-09-03 20:42:34 +0900

    유즈케이스 요청시 Loading 상태를 포함한 Flow를 반환하는 추상 클래스 및 함수 추가

--- modules/core-arch/src/main/java/com/editor/appcha/core/arch/coroutines/flow/ResultFlow.kt
+++ modules/core-arch/src/main/java/com/editor/appcha/core/arch/coroutines/flow/ResultFlow.kt
@@ -1,28 +1,54 @@
 package com.editor.appcha.core.arch.coroutines.flow
 
 import com.editor.appcha.core.arch.Result
-import com.editor.appcha.core.arch.buildResultCatching
 import kotlinx.coroutines.InternalCoroutinesApi
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.FlowCollector
 import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.flow.flow
 
 private class ResultFlow<T>(
-    private val block: suspend () -> T
+    private val loading: Boolean,
+    private val block: suspend FlowCollector<T>.() -> Unit
 ) : Flow<Result<T>> {
 
     @InternalCoroutinesApi
     override suspend fun collect(collector: FlowCollector<Result<T>>) {
-        val result = buildResultCatching { block() }
-        collector.emit(result)
+        if (loading) {
+            collector.emit(Result.Loading)
+        }
+        flow(block)
+            .catch { throwable -> collector.emit(Result.failure(throwable)) }
+            .collect { value -> collector.emit(Result.success(value)) }
     }
 }
 
+/**
+ * resultFlow<Int> {
+ *     emit(5)
+ *     emit(10)
+ * }
+ * -> Result.Loading, Result.success(5), Result.success(10)
+ */
 fun <T> resultFlow(
-    block: suspend () -> T
-): Flow<Result<T>> = ResultFlow(block)
+    loading: Boolean = true,
+    block: suspend FlowCollector<T>.() -> Unit
+): Flow<Result<T>> = ResultFlow(loading, block)
 
-fun <T> Flow<T>.asResultFlow(): Flow<Result<T>> =
-    map { value -> Result.success(value) }
-        .catch { throwable -> emit(Result.failure(throwable)) }
+
+/**
+ * resultFlowOf<Int>(5, 10) // Result.Loading, Result.success(5), Result.success(10)
+ */
+fun <T> resultFlowOf(vararg elements: T): Flow<Result<T>> = resultFlow {
+    for (element in elements) {
+        emit(element)
+    }
+}
+
+/**
+ * flowOf(5, 10).asResultFlow // Result.Loading, Result.success(5), Result.success(10)
+ */
+fun <T> Flow<T>.asResultFlow(): Flow<Result<T>> = resultFlow {
+    collect { value -> emit(value) }
+}
