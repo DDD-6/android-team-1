30eff5a3f49129a73a1c9ed38acf462bc2b34955
laco-dev <laco2951@gmail.com>
2021-09-15 19:12:35 +0900

    Add MaterialTheme with Color, Shape, Typography

---
+++ modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/theme/Color.kt
@@ -0,0 +1,28 @@
+package com.editor.appcha.ui.compose.theme
+
+import androidx.compose.material.lightColors
+import androidx.compose.ui.graphics.Color
+
+val Purple200 = Color(0xFFBB86FC)
+val Purple500 = Color(0xFF6200EE)
+val Purple700 = Color(0xFF3700B3)
+val Teal200 = Color(0xFF03DAC5)
+
+val AppChaColors = lightColors(
+    primary = Purple500,
+    primaryVariant = Purple700,
+    secondary = Teal200
+
+    /* Other default colors to override
+    background = Color.White,
+    surface = Color.White,
+    onPrimary = Color.White,
+    onSecondary = Color.Black,
+    onBackground = Color.Black,
+    onSurface = Color.Black,
+    */
+)
+
+
+
+// TODO: 컬러 시스템 구축
