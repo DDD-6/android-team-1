245284b5982d6121ef2660b25682dc885fb97cdb
peterchoee <peter.choee@gmail.com>
2021-09-18 02:11:07 +0900

    HIlt Local Module 설정

--- modules/local/src/main/java/com/editor/appcha/local/di/Module.kt
+++ modules/local/src/main/java/com/editor/appcha/local/di/Module.kt
@@ -2,5 +2,17 @@ package com.editor.appcha.local.di
 
 import com.editor.appcha.data.source.GreeterLocalDataSource
 import com.editor.appcha.local.source.GreeterLocalDataSourceImpl
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
 
-fun provideGreeterLocalDataSource(): GreeterLocalDataSource = GreeterLocalDataSourceImpl()
+@InstallIn(SingletonComponent::class)
+@Module
+abstract class Module {
+
+    @Binds
+    abstract fun bindGreeterLocalDataSource(
+        greeterLocalDataSourceImpl: GreeterLocalDataSourceImpl
+    ): GreeterLocalDataSource
+}
