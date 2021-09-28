c8d62c858934e7b13c2aff1c9fd327c3a6d05d1d
laco-dev <laco2951@gmail.com>
2021-09-28 23:06:24 +0900

    Add Hilt-compiler to data and remote module

--- modules/data/build.gradle.kts
+++ modules/data/build.gradle.kts
@@ -1,11 +1,13 @@
 plugins {
     `kotlin-jvm`
+    `kotlin-kapt`
 }
 
 dependencies {
     implementation(project(Dependencies.Module.DOMAIN))
 
     implementation(Dependencies.HILT_CORE)
+    kapt(Dependencies.HILT_COMPILER)
 
     implementation(Dependencies.COROUTINES_CORE)
 }
