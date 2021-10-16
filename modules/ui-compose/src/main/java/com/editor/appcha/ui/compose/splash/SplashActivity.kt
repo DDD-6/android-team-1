d9b1507f54efc0d85caaec82ac86c79f53d95631
laco-dev <laco2951@gmail.com>
2021-10-16 17:48:05 +0900

    스플래시에서 로그인 여부를 판단해 이벤트 발행하는 기능 추가

--- modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/splash/SplashActivity.kt
+++ modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/splash/SplashActivity.kt
@@ -1,15 +1,18 @@
 package com.editor.appcha.ui.compose.splash
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
-import com.editor.appcha.core.ui.event.EmptyViewEvent
 import com.editor.appcha.ui.compose.base.BaseActivity
+import com.editor.appcha.ui.compose.splash.SplashViewModel.Event
 import com.editor.appcha.ui.compose.theme.AppTheme
 import dagger.hilt.android.AndroidEntryPoint
 
+@SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
-class SplashActivity : BaseActivity<SplashViewModel, EmptyViewEvent>() {
+class SplashActivity : BaseActivity<SplashViewModel, Event>() {
 
     override val vm: SplashViewModel by viewModels()
 
@@ -23,7 +26,20 @@ class SplashActivity : BaseActivity<SplashViewModel, EmptyViewEvent>() {
         }
     }
 
-    override fun handleEvent(event: EmptyViewEvent) = Unit
+    override fun handleEvent(event: Event) = when (event) {
+        Event.StartMain -> startMainActivity()
+        Event.StartSignIn -> startSignInActivity()
+    }
+
+    private fun startMainActivity() {
+        Log.i(TAG, "startMainActivity")
+        // TODO: MainActivity 시작
+    }
+
+    private fun startSignInActivity() {
+        Log.i(TAG, "startSignInActivity")
+        // TODO: SignInActivity 시작
+    }
 }
 
 
