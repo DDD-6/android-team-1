82e97a52a7d7a8b1682a9183b729b61d7049abc9
laco-dev <laco2951@gmail.com>
2021-10-16 18:48:55 +0900

    스플래시에서 메인 화면으로 이동하는 로직 추가

--- modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/splash/SplashActivity.kt
+++ modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/splash/SplashActivity.kt
@@ -6,17 +6,23 @@ import android.util.Log
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.ui.graphics.Color
+import com.editor.appcha.navigation.MainNavigator
+import com.editor.appcha.ui.compose.R
 import com.editor.appcha.ui.compose.base.BaseActivity
 import com.editor.appcha.ui.compose.splash.SplashViewModel.Event
 import com.editor.appcha.ui.compose.theme.AppTheme
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
 import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
 @SuppressLint("CustomSplashScreen")
 @AndroidEntryPoint
 class SplashActivity : BaseActivity<SplashViewModel, Event>() {
 
     override val vm: SplashViewModel by viewModels()
+
+    @Inject
+    lateinit var mainNavigator: MainNavigator
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,8 +43,9 @@ class SplashActivity : BaseActivity<SplashViewModel, Event>() {
     }
 
     private fun startMainActivity() {
-        Log.i(TAG, "startMainActivity")
-        // TODO: MainActivity 시작
+        mainNavigator.launch(this)
+        overridePendingTransition(R.anim.push_enter, R.anim.push_exit)
+        finish()
     }
 
     private fun startSignInActivity() {
