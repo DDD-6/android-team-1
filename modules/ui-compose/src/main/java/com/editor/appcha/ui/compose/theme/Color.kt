bd75c3801670608e2dc04c0765fbe3c4ea57e491
laco-dev <laco2951@gmail.com>
2021-10-16 18:34:20 +0900

    Gray6 컬러 추가

--- modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/theme/Color.kt
+++ modules/ui-compose/src/main/java/com/editor/appcha/ui/compose/theme/Color.kt
@@ -12,6 +12,7 @@ val Gray2 = Color(0xFFCDD2D7)
 val Gray3 = Color(0xFF8C96A0)
 val Gray4 = Color(0xFF505A64)
 val Gray5 = Color(0xFF30323C)
+val Gray6 = Color(0xFF30323C)
 
 val Primary = Color(0xFF6ACDC8)
 val PrimaryLight = Color(0xFF6ACDC8)
@@ -26,6 +27,7 @@ class AppColors(
     gray3: Color = Gray3,
     gray4: Color = Gray4,
     gray5: Color = Gray5,
+    gray6: Color = Gray6,
     primary: Color = Primary,
     primaryLight: Color = PrimaryLight,
     statusError: Color = StatusError,
@@ -40,6 +42,8 @@ class AppColors(
     var gray4 by mutableStateOf(gray4)
         private set
     var gray5 by mutableStateOf(gray5)
+        private set
+    var gray6 by mutableStateOf(gray6)
         private set
     var primary by mutableStateOf(primary)
         private set
@@ -56,6 +60,7 @@ class AppColors(
         gray3: Color = this.gray3,
         gray4: Color = this.gray4,
         gray5: Color = this.gray5,
+        gray6: Color = this.gray6,
         primary: Color = this.primary,
         primaryLight: Color = this.primaryLight,
         statusError: Color = this.statusError,
@@ -66,6 +71,7 @@ class AppColors(
         gray3 = gray3,
         gray4 = gray4,
         gray5 = gray5,
+        gray6 = gray6,
         primary = primary,
         primaryLight = primaryLight,
         statusError = statusError,
