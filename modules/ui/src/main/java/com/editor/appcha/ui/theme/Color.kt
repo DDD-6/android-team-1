b82ef2a7138836976f1783e905e9ce5ed8784e5a
laco-dev <laco2951@gmail.com>
2021-10-30 17:02:54 +0900

    Typo, Colors 값 재정의

--- modules/ui/src/main/java/com/editor/appcha/ui/theme/Color.kt
+++ modules/ui/src/main/java/com/editor/appcha/ui/theme/Color.kt
@@ -7,15 +7,15 @@ import androidx.compose.runtime.setValue
 import androidx.compose.runtime.staticCompositionLocalOf
 import androidx.compose.ui.graphics.Color
 
-val Gray1 = Color(0xFFF0F0F0)
-val Gray2 = Color(0xFFCDD2D7)
-val Gray3 = Color(0xFF8C96A0)
-val Gray4 = Color(0xFF505A64)
-val Gray5 = Color(0xFF30323C)
+val Gray1 = Color(0xFFF9F9FB)
+val Gray2 = Color(0xFFE9EDF1)
+val Gray3 = Color(0xFFC5CDD4)
+val Gray4 = Color(0xFF8C96A0)
+val Gray5 = Color(0xFF5A646D)
 val Gray6 = Color(0xFF30323C)
 
 val Primary = Color(0xFF6ACDC8)
-val PrimaryLight = Color(0xFF6ACDC8)
+val PrimaryLight = Color(0xFFEDF8F7)
 
 val StatusError = Color(0xFFEB4B4B)
 val StatusCaution = Color(0xFFFF8E3C)
