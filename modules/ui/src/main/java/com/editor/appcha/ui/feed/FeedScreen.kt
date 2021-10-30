cf2c9068161eb9fcf05b38db41f3efe67593afb5
laco-dev <laco2951@gmail.com>
2021-10-30 14:59:10 +0900

    FeedScreen TODO 추가 및 불필요한 로그 삭제

--- modules/ui/src/main/java/com/editor/appcha/ui/feed/FeedScreen.kt
+++ modules/ui/src/main/java/com/editor/appcha/ui/feed/FeedScreen.kt
@@ -1,6 +1,5 @@
 package com.editor.appcha.ui.feed
 
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.itemsIndexed
@@ -12,9 +11,8 @@ import com.editor.appcha.ui.component.AppText
 @Composable
 fun FeedScreen() {
     val viewModel = hiltViewModel<FeedViewModel>()
-    Log.w("Melon", "ViewModel hash: ${viewModel}")
 
-    println("AA")
+    // TODO: UI 구현
     val items = List(100) { 0 }
     LazyColumn(modifier = Modifier.fillMaxWidth()) {
         itemsIndexed(items) { idx, item ->
