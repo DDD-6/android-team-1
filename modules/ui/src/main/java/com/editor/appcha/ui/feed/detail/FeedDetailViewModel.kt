9bc5ffaab9a3570afe6b8fad4c316f368633814c
laco-dev <laco2951@gmail.com>
2021-11-06 00:08:55 +0900

    :white_check_mark: Bundle로 받은 feedId가 없으면 getFeedUseCase를 호출하지 않는다

--- modules/ui/src/main/java/com/editor/appcha/ui/feed/detail/FeedDetailViewModel.kt
+++ modules/ui/src/main/java/com/editor/appcha/ui/feed/detail/FeedDetailViewModel.kt
@@ -1,5 +1,6 @@
 package com.editor.appcha.ui.feed.detail
 
+import androidx.lifecycle.SavedStateHandle
 import com.editor.appcha.core.arch.Result
 import com.editor.appcha.domain.usecase.GetFeedUseCase
 import com.editor.appcha.ui.base.BaseViewModel
@@ -12,17 +13,29 @@ import javax.inject.Inject
 
 @HiltViewModel
 class FeedDetailViewModel @Inject constructor(
-    private val getFeed: GetFeedUseCase
+    private val getFeed: GetFeedUseCase,
+    savedStateHandle: SavedStateHandle
 ) : BaseViewModel<EmptyViewEvent, State>(State()) {
 
     data class State(val feed: FeedDetailModel? = null) : ViewState
 
     init {
+        val feedId: String? = savedStateHandle.get(KEY_FEED_ID)
+        if (!feedId.isNullOrEmpty()) {
+            fetchFeed(feedId)
+        }
+    }
+
+    private fun fetchFeed(feedId: String) {
         launch {
-            when (val result = getFeed("")) {
+            when (val result = getFeed(feedId)) {
                 is Result.Success -> updateState { it.copy(feed = FeedDetailModel(result.value)) }
                 is Result.Failure -> updateState { State() }
             }
         }
+    }
+
+    companion object {
+        const val KEY_FEED_ID = "KEY_FEED_ID"
     }
 }
--- modules/ui/src/test/java/com/editor/appcha/ui/feed/detail/FeedDetailViewModelTest.kt
+++ modules/ui/src/test/java/com/editor/appcha/ui/feed/detail/FeedDetailViewModelTest.kt
@@ -1,11 +1,13 @@
 package com.editor.appcha.ui.feed.detail
 
+import androidx.lifecycle.SavedStateHandle
 import com.editor.appcha.core.arch.Result
 import com.editor.appcha.domain.model.FeedDetail
 import com.editor.appcha.domain.usecase.GetFeedUseCase
 import com.editor.appcha.ui.BaseTest
 import com.google.common.truth.Truth.assertThat
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
@@ -19,12 +21,30 @@ class FeedDetailViewModelTest : BaseTest() {
 
     @Test
     fun `ViewModel을 초기화 하면 FeedDetail을 불러온다`() = runBlockingTest {
+        // given
         coEvery { getFeedUseCase(any()) } returns Result.success(feedDetail())
+        val savedStateHandle = SavedStateHandle(mapOf(FeedDetailViewModel.KEY_FEED_ID to "1"))
 
-        val viewModel = FeedDetailViewModel(getFeedUseCase)
-
+        // when
+        val viewModel = FeedDetailViewModel(getFeedUseCase, savedStateHandle)
         val actual = viewModel.state.first().feed
+
+        // then
         assertThat(actual).isNotNull()
+    }
+
+    @Test
+    fun `Bundle로 받은 feedId가 없으면 getFeedUseCase를 호출하지 않는다`() = runBlockingTest {
+        // given
+        val id = ""
+        coEvery { getFeedUseCase(id) } returns Result.success(feedDetail(id = id))
+        val savedStateHandle = SavedStateHandle(mapOf(FeedDetailViewModel.KEY_FEED_ID to id))
+
+        // when
+        FeedDetailViewModel(getFeedUseCase, savedStateHandle)
+
+        // then
+        coVerify(exactly = 0) { getFeedUseCase(id) }
     }
 
     private fun feedDetail(
