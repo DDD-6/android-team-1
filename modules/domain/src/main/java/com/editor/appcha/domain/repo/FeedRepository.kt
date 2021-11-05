79b1e22e8e3bf8c821caadd01c606aa96f68fa7a
laco-dev <laco2951@gmail.com>
2021-11-05 23:10:03 +0900

    FeedDetail 인터페이스, 구현체 추가

--- modules/domain/src/main/java/com/editor/appcha/domain/repo/FeedRepository.kt
+++ modules/domain/src/main/java/com/editor/appcha/domain/repo/FeedRepository.kt
@@ -2,8 +2,11 @@ package com.editor.appcha.domain.repo
 
 import com.editor.appcha.core.arch.Result
 import com.editor.appcha.domain.model.Feed
+import com.editor.appcha.domain.model.FeedDetail
 
 interface FeedRepository {
 
     suspend fun getFeeds(): Result<List<Feed>>
+
+    suspend fun getFeed(feedId: String): Result<FeedDetail>
 }
