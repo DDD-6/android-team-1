359339256aa01074372b4dcb390e135fcc3af22b
laco-dev <laco2951@gmail.com>
2021-11-05 23:43:56 +0900

    FeedDetailModel 모델 추가

---
+++ modules/ui/src/main/java/com/editor/appcha/ui/model/FeedDetailModel.kt
@@ -0,0 +1,38 @@
+package com.editor.appcha.ui.model
+
+import com.editor.appcha.domain.model.FeedDetail
+
+data class FeedDetailModel(
+    val id: String,
+    val title: String,
+    val author: String,
+    val bodies: List<Body>,
+    val isFavorite: Boolean
+) {
+
+    sealed class Body {
+
+        data class Text(val text: String) : Body()
+
+        data class Image(val imageUrl: String) : Body()
+
+        data class Apps(val apps: List<AppModel>) : Body()
+    }
+
+    companion object {
+
+        operator fun invoke(domain: FeedDetail): FeedDetailModel = FeedDetailModel(
+            id = domain.id,
+            title = domain.title,
+            author = domain.author,
+            bodies = domain.bodies.map { body ->
+                when (body) {
+                    is FeedDetail.Body.Text -> Body.Text(body.text)
+                    is FeedDetail.Body.Image -> Body.Image(body.imageUrl)
+                    is FeedDetail.Body.Apps -> Body.Apps(body.apps.map { AppModel(it) })
+                }
+            },
+            isFavorite = domain.isFavorite
+        )
+    }
+}
