76ab43a822b2d2b61419bbdeec4898644424edb2
laco-dev <laco2951@gmail.com>
2021-11-06 15:23:02 +0900

    글쓰기 버튼 컴포저블 추가

--- modules/ui/src/main/java/com/editor/appcha/ui/community/CommunityScreen.kt
+++ modules/ui/src/main/java/com/editor/appcha/ui/community/CommunityScreen.kt
@@ -1,5 +1,6 @@
 package com.editor.appcha.ui.community
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
@@ -7,16 +8,23 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Divider
+import androidx.compose.material.Icon
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import com.editor.appcha.ui.R
 import com.editor.appcha.ui.component.AppText
 import com.editor.appcha.ui.model.BoardModel
 import com.editor.appcha.ui.theme.AppTheme
@@ -33,9 +41,24 @@ fun CommunityScreen(viewModel: CommunityViewModel) {
                 modifier = Modifier.align(Alignment.Center)
             )
         } else {
+            // 게시글
             BoardItems(
                 boards = boards,
                 onClick = viewModel::navigateToDetail
+            )
+            // 글쓰기 아이콘
+            Icon(
+                painter = painterResource(id = R.drawable.ic_pencil_simple),
+                contentDescription = null,
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .padding(end = 24.dp, bottom = 24.dp)
+                    .size(48.dp)
+                    .clip(CircleShape)
+                    .background(AppTheme.colors.primary)
+                    .clickable { viewModel.navigateToPost() }
+                    .padding(12.dp),
+                tint = Color.White
             )
         }
     }
