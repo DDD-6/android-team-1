8153fcda4d1c958d1b7c3e4bf9928e3348fa99fe
laco-dev <laco2951@gmail.com>
2021-11-06 12:11:17 +0900

    FeedDetail Thumbnail 추가

--- modules/ui/src/main/java/com/editor/appcha/ui/feed/detail/FeedDetailScreen.kt
+++ modules/ui/src/main/java/com/editor/appcha/ui/feed/detail/FeedDetailScreen.kt
@@ -2,6 +2,8 @@ package com.editor.appcha.ui.feed.detail
 
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
@@ -22,6 +24,8 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.editor.appcha.ui.R
+import com.editor.appcha.ui.component.NetworkImage
+import com.editor.appcha.ui.feed.FeedTitleAndAuthor
 import com.editor.appcha.ui.theme.AppTheme
 import com.google.accompanist.insets.statusBarsPadding
 
@@ -34,6 +38,14 @@ fun FeedDetailScreen(viewModel: FeedDetailViewModel = viewModel()) {
         if (feed == null) {
             CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
         } else {
+            Column {
+                FeedDetailThumbnail(
+                    imageUrl = feed.imageUrl,
+                    title = feed.title,
+                    author = feed.author,
+                    modifier = Modifier.aspectRatio(360f / 472f)
+                )
+            }
             TopSection(
                 isFavorite = feed.isFavorite,
                 onClose = viewModel::navigateUp,
@@ -82,6 +94,22 @@ private fun TopSection(
                 .clip(CircleShape)
                 .clickable { onFavorite() }
                 .padding(4.dp)
+        )
+    }
+}
+
+@Composable
+private fun FeedDetailThumbnail(
+    imageUrl: String,
+    title: String,
+    author: String,
+    modifier: Modifier = Modifier,
+) {
+    Box(modifier = modifier) {
+        NetworkImage(data = imageUrl)
+        FeedTitleAndAuthor(
+            title = title, author = author,
+            modifier = Modifier.align(Alignment.BottomStart)
         )
     }
 }
